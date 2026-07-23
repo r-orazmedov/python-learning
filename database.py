ec5d7ff2da9322a0a3ef11de0bd30b1963c4a8b4
Rahat Orazmedov <rahatgurbanovic@gmail.com>
2026-07-23 16:59:29 +0500

    Первый CRUD-проект

--- database.py
+++ database.py
@@ -36,6 +36,11 @@ def show_students():
 
     students = cursor.fetchall()
 
+    if not students:
+        print("No students found.")
+        connection.close()
+        return
+
     print("===== List of students =====")
 
     for student in students:
@@ -44,4 +49,50 @@ def show_students():
         print(f"Age: {student[2]}")
         print("-" * 30)
 
+    connection.close()
+
+def find_student(name):
+    connection = sqlite3.connect("students.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    SELECT * FROM students 
+    WHERE name = ?
+    """, (name,))
+
+    students = cursor.fetchall()
+
+    if not students:
+        print("Student not found.")
+        connection.close()
+        return
+
+    for student in students:
+        print(f"\nID: {student[0]}")
+        print(f"Name: {student[1]}")
+        print(f"Age: {student[2]}")
+
+    connection.close()
+
+def update_student(student_id, new_name, new_age):
+    connection = sqlite3.connect("students.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    UPDATE students
+    SET name = ?, age = ?
+    WHERE id = ?""", (new_name, new_age, student_id))
+
+    connection.commit()
+    connection.close()
+
+def delete_student(student_id):
+    connection = sqlite3.connect("students.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    DELETE FROM students
+    WHERE id = ?""", (student_id,))
+
+    connection.commit()
     connection.close()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from database import create_database, add_student, show_students
+from database import create_database, add_student, show_students, find_student, update_student, delete_student
 
 create_database()
 
@@ -6,7 +6,10 @@ while True:
     print("\n===== Student Management System =====")
     print("1. Add Student")
     print("2. List Students")
-    print("3. Exit")
+    print("3. Find Student")
+    print("4. Update Student")
+    print("5. Delete Student")
+    print("6. Exit")
 
     choice = input("Choose: ")
 
@@ -22,6 +25,27 @@ while True:
         show_students()
 
     elif choice == "3":
+        name = input("Name: ")
+
+        find_student(name)
+
+    elif choice == "4":
+        student_id = int(input("Student ID: "))
+        new_name = input("New Name: ")
+        new_age = int(input("New Age: "))
+
+        update_student(student_id, new_name, new_age)
+
+        print("Student updated!")
+
+    elif choice == "5":
+        student_id = int(input("Student ID: "))
+
+        delete_student(student_id)
+
+        print("Student deleted!")
+
+    elif choice == "6":
         print("Goodbye!")
         break
 
